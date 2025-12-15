fe25b64087acad414ed7a2ae1a1f99dd7d3d50ab
afterrender1 <afterrender1@gmail.com>
2025-12-15 19:34:43 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,12 +10,8 @@ dotenv.config()
 
 
 const app = express();
-app.use(cors(
-    {
-        origin : "http://localhost:3000",
-        credentials : true 
-    }
-));
+app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser())
 app.use("/uploads", express.static(path.join(process.cwd(), "/public/uploads")));
