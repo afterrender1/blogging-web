11f911c2f66c3fe9ac00b7617f1c9178a9c750e5
afterrender1 <afterrender1@gmail.com>
2025-12-16 14:39:17 +0500

    Update Navbar.jsx

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -163,7 +163,7 @@ export default function Navbar() {
   );
 
   return (
-    <nav className="bg-white border-b border-gray-100 p-2 sticky top-0 z-50 shadow-sm">
+    <nav className="bg-[#F8F9F8] border-b border-gray-100 p-2 sticky top-0 z-50 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
